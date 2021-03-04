c9200d12b438d17d751a0410fd597f8cff04fe14
Soujiro17 <vicente_tomi@hotmail.com>
2021-03-04 17:46:32 -0300

    a ver si se arregla la descripcion y titulo

--- server.js
+++ server.js
@@ -31,11 +31,7 @@ app.get('/', function (req, res) {
     if (err) {
       return console.log(err);
     }
-
-    data = data
-      .replace(/__TITLE__/g, "Calcula mi promedio")
-      .replace(/__DESCRIPTION__/g, "Calcula tu promedio de forma fácil y sencilla! También contamos con tablas de frecuencia con datos ordenados y no.");
-
+    
     res.send(data)
   });
 });
